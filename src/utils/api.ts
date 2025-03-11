17e5534c41a15df418d09e04cd5a5b70b7a51b16
muritador5050 <muritador5050@gmail.com>
2025-03-11 17:39:39 +0100

    i added api key in .env file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,19 +1,16 @@
 // src/utils/api.ts
 export async function fetchExchangeRates(): Promise<{ [key: string]: number }> {
-  const apiKey = 'cur_live_s6VNvcqYp21OJtxKrUUzShPtFJXFZ0AWxKdameLy';
+  const apiKey = process.env.REACT_APP_API_KEY;
   const currencies = 'EUR,USD,CAD,GBP,JPY,AUD,NGN';
 
   const url = `https://api.currencyapi.com/v3/latest?apikey=${apiKey}&currencies=${currencies}`;
-
   try {
     const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     const data = await response.json();
-
     if (data && data.data) {
       const rates: { [key: string]: number } = {};
       for (const currency in data.data) {
